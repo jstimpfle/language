1a6971d104f7179031f77ac93d3fe31740829a72
Jens Stimpfle <jfs@jstimpfle.de>
2018-11-18 14:10:47 +0100

    a little improvement

--- x64asm.c
+++ x64asm.c
@@ -56,17 +56,27 @@ void end_symbol(void)
 }
 
 INTERNAL
-void emit_data(uchar *buf, int len)
-{
-}
-
-INTERNAL
-void emit_code(uchar *buf, int len)
+void emit_bytes(uchar *buf, int len)
 {
         int pos = codeSectionCnt;
         codeSectionCnt += len;
         RESIZE_GLOBAL_BUFFER(codeSection, codeSectionCnt);
         copy_mem(codeSection + pos, buf, len);
+}
+
+INTERNAL
+void emit(Constval c)
+{
+        assert(-128 <= c && c < 128);
+        uchar b = c;
+        emit_bytes(&b, 1);
+}
+
+#define BYTE(x, n) ((unsigned long long) (x) >> (8*(n)))
+void emit4(Constval c)
+{
+        uchar bs[] = { BYTE(c, 0), BYTE(c, 1), BYTE(c, 2), BYTE(c, 3) };
+        emit_bytes(bs, 4);
 }
 
 INTERNAL
@@ -79,47 +89,30 @@ void emit_code_relocation(Symbol symbol, int offset)
         relocInfo[reloc].offset = offset;
 }
 
-#define BYTE(x, n) ((unsigned long long) (x) >> (8*(n)))
 #define EMIT(...) do { \
         uchar bytes[] = { __VA_ARGS__ }; \
-        emit_code(bytes, sizeof bytes); \
+        emit_bytes(bytes, sizeof bytes); \
 } while (0)
 
-INTERNAL
-void emit_mov_64_imm_reg(Constval imm, int x64reg)
+static int make_modrm_byte(unsigned mod, unsigned reg, unsigned rm)
 {
-#if 0
-        EMIT(
-                0x4c, 0x01, (x64reg<<0) | 0x00,
-                /* up to 3 bytes imm */
-                BYTE(imm, 0), BYTE(imm, 1), BYTE(imm, 2)
-        );
-#endif
-        EMIT(
-                0x00, 0x00,
-                0x01, 0x00,
-                0x02, 0x00,
-                0x03, 0x00,
-                0x04, 0x00,
-
-                      0x89, 0x00,
-                0x4c, 0x89, 0x00
-        );
+        assert(mod < 4);
+        assert(reg < 8);
+        assert(rm < 8);
+        return (mod << 6) | (reg << 3) | rm;
 }
 
 #define REX_BASE 0x40
 #define REX_W    0x08  /* usually means "64-bit operand instead of default size" */
-#define REX_R    0x04  /* usually shift register range of 1st operand to R8-... from RAX-... */
-#define REX_X    0x02
-#define REX_B    0x01  /* usually shift register range of 2st operand to R8-... from RAX-... */
+#define REX_R    0x04  /* Extension of the ModR/M reg field */
+#define REX_X    0x02  /* Extension of the SIB index field */
+#define REX_B    0x01  /* Extension of the ModR/M r/m field, SIB base field, or Opcode reg field */
 
-/* Emit the MOD-REG-R/M byte and the optional displacement byte(s).
+/* Emit the MOD-REG-R/M byte and the optional SIB and displacement byte(s).
  *
  * r1 and r2 are interpreted without the 4th LSB,
  * such that they are always one of the low 8 registers. (The 4th LSB is encoded
- * in a different place in the x64 ISA). After that reinterpretation, r2 must
- * not be X64REG_RBP -- that value is taken for SIB addressing (scale index
- * byte).
+ * in a different place in the x64 ISA).
  *
  * d is the displacement (register relative offset, which applies to r1 or to r2
  * depending on the preceding opcode). The value of d determines the 2 MSB of
@@ -137,12 +130,17 @@ static inline int emit_modrmreg_and_displacement_bytes(int r1, int r2, long d)
 {
         r1 &= 7;
         r2 &= 7;
-        assert(0 <= r1 && r1 < 8);
-        assert(0 <= r2 && r2 < 8 && r2 != X64REG_RBP);
 
         int mod;
-        if (d == 0)
+        int sib;
+        int havesib = 0;
+
+        /* RBP indirect with 0 displacement is not possible with a mod=0x00
+         * encoding I think. So we make a single byte displacement in this case.
+         */
+        if (d == 0 && r2 != X64REG_RBP) {
                 mod = 0x00;
+        }
         else if (-128 <= d && d < 128) {
                 mod = 0x01;
         }
@@ -152,30 +150,56 @@ static inline int emit_modrmreg_and_displacement_bytes(int r1, int r2, long d)
                 mod = 0x02;
         }
 
-        int modrmreg = (mod << 6) | (r1 << 3) | r2;
+        if (r2 == X64REG_RSP) {
+                havesib = 1;
+                sib = X64REG_RSP;
+        }
+        else if (r2 == X64REG_RBP && mod == 0x00) {
+                mod = 0x01;
+        }
 
-        EMIT(modrmreg);
+        int modrm = make_modrm_byte(mod, r1, r2);
+        EMIT(modrm);
+
+        if (havesib)
+                emit(sib);
+
         if (mod == 0x01)
-                EMIT(d);
-        else if (mod == 0x02)
-                EMIT(BYTE(d, 0), BYTE(d, 1), BYTE(d, 2), BYTE(d, 3));
+                emit(d);
+        else if (mod == 0x02) {
+                emit4(d);
+        }
+}
+
+INTERNAL
+void emit_mov_64_imm_reg(Constval imm, int r1, long long d)
+{
+        /*
+        EMIT(0x48, 0xb8);
+        emit_modrmreg_and_displacement_bytes(X64REG_RAX, r1, 10);
+        */
+}
+
+INTERNAL
+void emit_mov_64_imm_RAX(Constval imm)
+{
+        EMIT(0x48, 0xb8);
+        emit4(imm);
 }
 
 INTERNAL
 void emit_add_64_imm_RAX(Constval imm)
 {
         assert(imm < (1ULL << 32));
-        EMIT(
-                REX_BASE|REX_W, 0x05,
-                BYTE(imm, 0), BYTE(imm, 1), BYTE(imm, 2), BYTE(imm, 3)
-        );
+        EMIT(REX_BASE|REX_W, 0x05);
+        emit4(imm);
 }
 
 INTERNAL
 void emit_add_64_reg_indirect(int r1, int r2, long d)
 {
-        int R = (r1 >= 8) ? REX_R : 0;
-        int B = (r2 >= 8) ? REX_B : 0;
+        int R = (r1 & ~7) ? REX_R : 0;
+        int B = (r2 & ~7) ? REX_B : 0;
         EMIT(REX_BASE|REX_W|R|B, 0x01);
         emit_modrmreg_and_displacement_bytes(r1, r2, d);
 }
@@ -183,27 +207,26 @@ void emit_add_64_reg_indirect(int r1, int r2, long d)
 INTERNAL
 void emit_add_64_indirect_reg(int r1, int r2, long d)
 {
-        int R = (r2 >= 8) ? REX_R : 0;
-        int B = (r1 >= 8) ? REX_B : 0;
+        int R = (r2 & ~7) ? REX_R : 0;
+        int B = (r1 & ~7) ? REX_B : 0;
         EMIT(REX_BASE|REX_W|R|B, 0x03);
-        emit_modrmreg_and_displacement_bytes(r1, r2, d);
+        emit_modrmreg_and_displacement_bytes(r2, r1, d);
 }
 
-void emit_mov_64_imm_indirect(Constval imm, int x64reg, long d)
+INTERNAL
+void emit_mov_64_reg_reg(int r1, int r2)
 {
-        /*
-        EMIT(
-                REX_BASE|REX_W, 0x8a,
-                BYTE(imm, 0), BYTE(imm, 1), BYTE(imm, 2), BYTE(imm, 3)
-        );
-        */
+        int R = (r1 & ~7) ? REX_R : 0;
+        int B = (r2 & ~7) ? REX_B : 0;
+        int modrm = make_modrm_byte(0x03, r1 & 7, r2 & 7);
+        EMIT(REX_BASE|REX_W|R|B, 0x89, modrm);
 }
 
 INTERNAL
 void emit_mov_64_reg_indirect(int r1, int r2, long d)
 {
-        int R = (r1 >= 8) ? REX_R : 0;
-        int B = (r2 >= 8) ? REX_B : 0;
+        int R = (r1 & ~7) ? REX_R : 0;
+        int B = (r2 & ~7) ? REX_B : 0;
         EMIT(REX_BASE|REX_W|R|B, 0x89);
         emit_modrmreg_and_displacement_bytes(r1, r2, d);
 }
@@ -212,10 +235,10 @@ void emit_mov_64_reg_indirect(int r1, int r2, long d)
 INTERNAL
 void emit_mov_64_indirect_reg(int r1, int r2, long d)
 {
-        int R = (r2 >= 8) ? REX_R : 0;
-        int B = (r1 >= 8) ? REX_B : 0;
+        int R = (r2 & ~7) ? REX_R : 0;
+        int B = (r1 & ~7) ? REX_B : 0;
         EMIT(REX_BASE|REX_W|R|B, 0x89|0x02);
-        emit_modrmreg_and_displacement_bytes(r1, r2, d);
+        emit_modrmreg_and_displacement_bytes(r2, r1, d);
 }
 
 INTERNAL
@@ -227,21 +250,27 @@ void emit_mov_64_reg_stack(int x64reg, X64StackLoc loc)
 INTERNAL
 void emit_load_constant_stack(Constval constval, X64StackLoc loc)
 {
-        emit_mov_64_imm_reg(constval, X64REG_RAX);
-        emit_mov_64_reg_stack(X64REG_RAX, loc);
+        emit_mov_64_imm_reg(constval, X64REG_RSP, loc);
 }
 
 INTERNAL
 void emit_load_symaddr_stack(Symbol symbol, X64StackLoc loc)
 {
         emit_code_relocation(symbol, codeSectionCnt);
-        emit_mov_64_imm_reg(0, X64REG_RAX);
+        emit_mov_64_imm_reg(0, X64REG_RAX, 0);
         emit_mov_64_reg_stack(X64REG_RAX, loc);
 }
 
 INTERNAL
 void emit_load_addrval_stack(Constval constval, X64StackLoc loc)
 {
+}
+
+INTERNAL
+void emit_function_epilogue(void)
+{
+        emit_mov_64_reg_reg(X64REG_RBP, X64REG_RSP);
+        EMIT(0xc3);
 }
 
 INTERNAL
@@ -286,26 +315,41 @@ void x64asm_proc(IrProc irp)
 			break;
                 case IRSTMT_GOTO:
 			break;
-                case IRSTMT_RETURN:
+                case IRSTMT_RETURN: {
+                        IrReturnResult result = irStmtInfo[irs].tReturn.firstResult;
+                        if (result != -1) {
+                                IrReg irreg = irReturnResultInfo[result].resultReg;
+                                X64StackLoc loc = find_stack_loc(irreg);
+                                loc = 0x13; //XXX test
+                                emit_mov_64_indirect_reg(X64REG_RSP, X64REG_RAX, loc);
+                        }
+                        goto funcreturn;
 			break;
+                }
+                default:
+                        UNHANDLED_CASE();
                 }
         }
 
+funcreturn:
+        emit_function_epilogue();
         end_symbol();
 }
 
 void codegen_x64(void)
 {
+        /*
         for (IrProc x = 0; x < irProcCnt; x++)
                 x64asm_proc(x);
+                */
 
         emit_add_64_imm_RAX(0x31);
         emit_add_64_indirect_reg(X64REG_RCX, X64REG_RDX, 0x3f3f3f3f);
-        emit_add_64_reg_indirect(X64REG_RCX, X64REG_RDX, 0);
-        emit_mov_64_reg_indirect(X64REG_RDX, X64REG_R11, 3);
-        emit_mov_64_imm_indirect(42, X64REG_RAX, 0);
-        emit_mov_64_indirect_reg(X64REG_RDX, X64REG_RBX, 127);
-        //emit_mov_64_reg_indirect(X64REG_RSP, X64REG_R13);
+        emit_add_64_reg_indirect(X64REG_RCX, X64REG_RDX, 0x27);
+        emit_mov_64_reg_reg(X64REG_R11, X64REG_RSP);
+        emit_mov_64_reg_indirect(X64REG_RDX, X64REG_RBP, 0x0);
+        emit_mov_64_indirect_reg(X64REG_RDX, X64REG_RBX, 0x2);
+        emit_mov_64_reg_indirect(X64REG_RSP, X64REG_RSP, 0x7);
 
         for (int i = 0; i < codeSectionCnt; i++) {
                 if (i & 7)
