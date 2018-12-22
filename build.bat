c91f159c520ebe79620deaecca0d82f86120d931
Jens Stimpfle <jfs@jstimpfle.de>
2018-12-22 18:15:18 +0100

    build.bat: add debug information

--- build.bat
+++ build.bat
@@ -2,4 +2,4 @@ rem Build script for Windows (MSVC compiler).
 rem You must setup the environment using vcvars64 before calling this script
 
 if not exist build mkdir build
-cl /Fe"build\language.exe" /I include\ src\*.c
+cl /Fe"build\language.exe" /I include\ src\*.c /Zi
