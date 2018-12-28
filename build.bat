dfc46cbb39280df41eea24e43ee952498ba6f8cb
Jens Stimpfle <jfs@jstimpfle.de>
2018-12-28 14:52:50 +0100

    do proper error checking in build.bat

--- build.bat
+++ build.bat
@@ -2,4 +2,15 @@ rem Build script for Windows (MSVC compiler).
 rem You must setup the environment using vcvars64 before calling this script
 
 if not exist build mkdir build
+
+@echo on
 cl /Fe"build\language.exe" /I include\ src\*.c /Zi
+
+@echo off
+if %ERRORLEVEL% NEQ 0 (
+    REM wtf everything about this environment is so illogical. Why can't I just
+    REM echo to get a newline?
+    echo.
+    echo COMPILER ERROR %ERRORLEVEL%
+    exit /B 1
+)
